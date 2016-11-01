48bb73d88b3f7598336eeda15f42145d46ed31df
Deepwater Creations <Dylan.Craine@DeepwaterCreations.com>
2016-11-01 13:26:36 -0600

    Whoops, I forgot dropping columns isn't in-place

--- src/build_generif_csv.py
+++ src/build_generif_csv.py
@@ -20,7 +20,7 @@ generifs = pd.DataFrame(np.array(datalist), columns=['Tax ID',
                                'last update',
                                'timestamp',
                                'GeneRIF text'])
-generifs.drop(['Tax ID', 'PubMed ID (PMID) list', 'last update', 'timestamp'], axis=1)
+generifs = generifs.drop(['Tax ID', 'PubMed ID (PMID) list', 'last update', 'timestamp'], axis=1)
 generifs['Gene ID'] = generifs['Gene ID'].astype(np.int64)
 
 #Combine the datasets
