4dcf87c52e159bf6cea5f4b7b8979ee13ec80e0c
Deepwater Creations <Dylan.Craine@DeepwaterCreations.com>
2016-11-01 16:28:08 -0600

    Add basic TfIdf vectorization to dataload module

--- src/dataload.py
+++ src/dataload.py
@@ -1,5 +1,12 @@
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 def get_gendr_rifs_df():
     df = pd.read_csv('data/gendr_rifs.csv')
     return df
+
+def get_tfidf():
+    df = get_gendr_rifs_df()
+    vectorizer = TfidfVectorizer()
+    corpus = df['GeneRIF text']
+    return vectorizer.fit_transform(corpus)
