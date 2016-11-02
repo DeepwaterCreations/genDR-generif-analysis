404bdeffe90b018cb39b7f2250e132607e1695bd
Deepwater Creations <Dylan.Craine@DeepwaterCreations.com>
2016-11-02 16:20:01 -0600

    Add NMF function
    This takes forever and a day to run. Maybe I need to tweak my TfIdf to
    cut down on the number of features.

--- src/unsupervised.py
+++ src/unsupervised.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.cluster import KMeans
+from sklearn.decomposition import NMF
 
 import dataload
 
@@ -8,8 +9,12 @@ def get_kmeans_clusters(data):
     clusters = model.fit_predict(data)
     return clusters
 
+def get_matrix_factorization(data):
+    model = NMF()
+    W = model.fit_transform(data)
+    print "Number of iterations:", model.n_iter_
+    return W
+
 if __name__ == "__main__":
     data = dataload.get_tfidf()
-    
-
-
+    print get_matrix_factorization(data)
