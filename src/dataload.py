d2ce194bae2d2054a07b5ce1e98371d863614142
Deepwater Creations <Dylan.Craine@DeepwaterCreations.com>
2016-11-04 11:40:59 -0600

    Allow get_tfidf to take a data source as an argument

--- src/dataload.py
+++ src/dataload.py
@@ -5,9 +5,10 @@ def get_gendr_rifs_df():
     df = pd.read_csv('data/gendr_rifs.csv')
     return df
 
-def get_tfidf():
-    df = get_gendr_rifs_df()
-    corpus = df['GeneRIF text']
+def get_tfidf(corpus=None):
+    if corpus is None:
+        data = get_gendr_rifs_df()
+        corpus = data['GeneRIF text']
     vectorizer = TfidfVectorizer(stop_words='english')
     vectorizer.fit(corpus)
     return vectorizer, vectorizer.transform(corpus).todense()
