3b429cc089be37d8f843cb6b38f163942d54086f
Deepwater Creations <Dylan.Craine@DeepwaterCreations.com>
2016-11-07 16:39:26 -0700

    Add function to generate a train/test split

--- src/dataload.py
+++ src/dataload.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import sklearn.model_selection
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 def get_gendr_rifs_df():
@@ -16,3 +18,20 @@ def get_tfidf(corpus=None):
     vectorizer = TfidfVectorizer(stop_words='english')
     vectorizer.fit(corpus)
     return vectorizer, vectorizer.transform(corpus)
+
+def get_train_test():
+    df = get_labeled_df()
+    _, tfidf = get_tfidf(df['GeneRIF text'])
+    #I have orders of magnitude more geneRIFs that aren't for genDR genes than ones that are,
+    #so I want to take a subset of the 'false' values and only train/test on those.
+    subset_rows = np.random.choice(df[df['in_genDR'] == 0].index.values, df.shape[0]/100)
+    subset_rows = np.concatenate((df[df['in_genDR'] == 1].index.values, subset_rows))
+    df = df.iloc[subset_rows]
+
+    y = df.pop('in_genDR').values
+    X = tfidf[subset_rows]
+
+    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, 
+                                                                            test_size = 0.2,
+                                                                            stratify = y)
+    return X_train, X_test, y_train, y_test
