ea1f97822cfa2dc7538ba70e011f27ef6365060b
Deepwater Creations <Dylan.Craine@DeepwaterCreations.com>
2016-11-07 13:12:11 -0700

    csv generator outputs in_genDR labels
    It also now adds all the generifs to the output, not just the ones that
    are in genDR.

--- src/build_generif_csv.py
+++ src/build_generif_csv.py
@@ -3,29 +3,46 @@ import re
 import pandas as pd
 import numpy as np
 
-#Load the GenDR data
-gendr = pd.read_csv('data/gendr_manipulations.csv')
+def load_gendr():
+    #Load the GenDR data
+    gendr = pd.read_csv('data/gendr_manipulations.csv')
+    return gendr
 
-#Load the significantly less nicely-formatted geneRIF data
-reg = re.compile(r"[\d:,-]+(?![^\t]+$)|[^\t]+$")
-datalist = []
-with open('data/generifs_basic') as datafile:
-    datafile.readline() #The first line is just column headers
-    for line in datafile:
-        values = reg.findall(line)
-        datalist.append(values)
-generifs = pd.DataFrame(np.array(datalist), columns=['Tax ID',
-                               'Gene ID',
-                               'PubMed ID (PMID) list',
-                               'last update',
-                               'timestamp',
-                               'GeneRIF text'])
-generifs = generifs.drop(['Tax ID', 'PubMed ID (PMID) list', 'last update', 'timestamp'], axis=1)
-generifs['Gene ID'] = generifs['Gene ID'].astype(np.int64)
+def load_generifs():
+    #Load the significantly less nicely-formatted geneRIF data
+    reg = re.compile(r"[\d:,-]+(?![^\t]+$)|[^\t]+$")
+    datalist = []
+    with open('data/generifs_basic') as datafile:
+        datafile.readline() #The first line is just column headers
+        for line in datafile:
+            values = reg.findall(line)
+            datalist.append(values)
+    generifs = pd.DataFrame(np.array(datalist), columns=['Tax ID',
+                                   'Gene ID',
+                                   'PubMed ID (PMID) list',
+                                   'last update',
+                                   'timestamp',
+                                   'GeneRIF text'])
+    generifs = generifs.drop(['Tax ID', 'PubMed ID (PMID) list', 'last update', 'timestamp'], axis=1)
+    generifs['Gene ID'] = generifs['Gene ID'].astype(np.int64)
+    return generifs
 
-#Combine the datasets
-gendr_rifs = pd.merge(gendr, generifs, left_on="entrez_id", right_on="Gene ID")
-gendr_rifs = gendr_rifs.drop(['entrez_id'], axis=1)
+def match_gendr_to_rifs(gendr, generifs):
+    #Combine the datasets
+    gendr_rifs = pd.merge(gendr, generifs,
+                        how="right",
+                        left_on="entrez_id",
+                        right_on="Gene ID",
+                        indicator="in_genDR")
+    gendr_rifs = gendr_rifs.drop(['entrez_id'], axis=1)
+    gendr_rifs['in_genDR'] = map(lambda x: 1 if x == "both" else 0, gendr_rifs['in_genDR'])
+    return gendr_rifs
 
-#Export to csv
-gendr_rifs.to_csv('data/gendr_rifs.csv')
+if __name__ == "__main__":
+    gendr = load_gendr()
+    generifs = load_generifs()
+    gendr_rifs = match_gendr_to_rifs(gendr, generifs)
+
+    #Export to csv
+    gendr_rifs.to_csv('data/gendr_rifs.csv')
+
--- src/dataload.py
+++ src/dataload.py
@@ -2,6 +2,10 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 def get_gendr_rifs_df():
+    df = get_labeled_df()
+    return df[df['in_genDR'] == 1]
+
+def get_labeled_df():
     df = pd.read_csv('data/gendr_rifs.csv')
     return df
 
