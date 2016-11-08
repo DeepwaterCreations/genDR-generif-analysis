0ea38a85ab0292df410a98bb559d586011cdd93f
Deepwater Creations <Dylan.Craine@DeepwaterCreations.com>
2016-11-08 15:08:08 -0700

    Modify subset data function to not do tfidf

--- src/dataload.py
+++ src/dataload.py
@@ -19,19 +19,16 @@ def get_tfidf(corpus=None):
     vectorizer.fit(corpus)
     return vectorizer, vectorizer.transform(corpus)
 
-def get_train_test():
+def get_subset_data():
     df = get_labeled_df()
-    _, tfidf = get_tfidf(df['GeneRIF text'])
     #I have orders of magnitude more geneRIFs that aren't for genDR genes than ones that are,
     #so I want to take a subset of the 'false' values and only train/test on those.
     subset_rows = np.random.choice(df[df['in_genDR'] == 0].index.values, df.shape[0]/100)
     subset_rows = np.concatenate((df[df['in_genDR'] == 1].index.values, subset_rows))
     df = df.iloc[subset_rows]
 
-    y = df.pop('in_genDR').values
-    X = tfidf[subset_rows]
+    y = df['in_genDR']
+    corpus = df['GeneRIF text']
 
-    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, 
-                                                                            test_size = 0.2,
-                                                                            stratify = y)
-    return X_train, X_test, y_train, y_test
+    return corpus, y
+
