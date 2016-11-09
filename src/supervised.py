5868993d2384605ade17aeb053195395755a7987
Deepwater Creations <Dylan.Craine@DeepwaterCreations.com>
2016-11-09 12:53:03 -0700

    Move gridsearch into a separate function

--- src/supervised.py
+++ src/supervised.py
@@ -7,11 +7,8 @@ from sklearn.model_selection import GridSearchCV
 
 import dataload
 
-if __name__ == "__main__":
+def do_gridsearch(X_train, y_train):
     pipeline = dataload.get_pipeline(MultinomialNB())
-    X, y = dataload.get_subset_data()
-    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, 
-                                                                            stratify = y)
     gridsearch = GridSearchCV(pipeline, {
         'tfidfvectorizer__max_df':(0.01, 0.1, 0.2, 0.5, 0.8, 1.0),
         # 'tfidfvectorizer__min_df':(0.0, 0.05, 0.1),
@@ -21,8 +18,17 @@ if __name__ == "__main__":
         'multinomialnb__alpha':(0.0, 0.001, 0.01, 0.25, 0.5, 0.75, 1.0)
         }, n_jobs=7, verbose=1)
     gridsearch.fit(X_train, y_train)
+    return gridsearch
+
+
+if __name__ == "__main__":
+    X, y = dataload.get_subset_data()
+    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, 
+                                                                            stratify = y)
+    gridsearch = do_gridsearch(X_train, y_train)
     print "SCORE:{0}".format(gridsearch.score(X_test, y_test))
     print "Best Estimator:{0}".format(gridsearch.best_estimator_)
     print "Best Params:{0}".format(gridsearch.best_params_)
+
     with open("data/gridsearch.pckl", 'w') as p_file:
         cPickle.dump(gridsearch, p_file)
