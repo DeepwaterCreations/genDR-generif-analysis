846e65827b80dd62e7989e19d620aa419b7f370c
Deepwater Creations <Dylan.Craine@DeepwaterCreations.com>
2016-11-10 00:58:07 -0700

    Make predictions on whole data set
    Saves to a .csv file.

--- src/run_model.py
+++ src/run_model.py
@@ -1,6 +1,20 @@
 import cPickle
 
 import dataload
+from build_model import MODEL_FILEPATH
+
+PREDICTIONS_FILEPATH = "data/predictions.csv"
 
 if __name__ == "__main__":
-    pass
+    with open(MODEL_FILEPATH) as model_file:
+        model = cPickle.load(model_file)
+    df = dataload.get_labeled_df()
+    df = df[df['in_genDR'] == 0]
+    
+    corpus = df["GeneRIF text"]
+
+    predictions = model.predict(corpus)
+
+    df['dr_relevant'] = predictions
+
+    df.to_csv(PREDICTIONS_FILEPATH)
