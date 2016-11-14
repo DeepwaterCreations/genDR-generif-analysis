a690c03bc6c11530930be6cea7096b1768752b97
Deepwater Creations <Dylan.Craine@DeepwaterCreations.com>
2016-11-13 19:31:13 -0700

    Implement function to return a model's top n words

--- src/run_model.py
+++ src/run_model.py
@@ -1,9 +1,18 @@
 import cPickle
 
+import numpy as np
+
 import dataload
 from build_model import MODEL_FILEPATH
 
 PREDICTIONS_FILEPATH = "data/predictions.csv"
+
+def get_top_words(model, n=50):
+    """Return a list of the top n most important words found by the model"""
+    coefs = model.named_steps['multinomialnb'].coef_
+    max_indices = coefs[0].argsort()
+    featurenames = model.named_steps['tfidfvectorizer'].get_feature_names()
+    return np.array(featurenames)[max_indices][:-(n+1):-1]
 
 if __name__ == "__main__":
     with open(MODEL_FILEPATH) as model_file:
