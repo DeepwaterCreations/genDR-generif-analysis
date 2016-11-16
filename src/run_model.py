d815534e4b0d9633917ed31f997d1c79b8668226
Deepwater Creations <Dylan.Craine@DeepwaterCreations.com>
2016-11-15 22:10:25 -0700

    Function to save word cloud for predictive model

--- src/run_model.py
+++ src/run_model.py
@@ -1,6 +1,7 @@
 import cPickle
 
 import numpy as np
+from wordcloud import WordCloud
 
 import dataload
 from build_model import MODEL_FILEPATH
@@ -13,6 +14,19 @@ def get_top_words(model, n=50):
     max_indices = coefs[0].argsort()
     featurenames = model.named_steps['tfidfvectorizer'].get_feature_names()
     return np.array(featurenames)[max_indices][:-(n+1):-1]
+
+def get_top_words_cloud(featurenames, coefficients, num_words=100):
+    weights = zip(featurenames, coefficients)
+    weights.sort(key=lambda x:x[1])
+    wc = WordCloud(background_color="white", 
+            ranks_only=True,
+            max_font_size=44,
+            prefer_horizontal=1.0,
+            width=800,
+            height=400,
+            max_words=100)
+    wc.fit_words(weights[:-101:-1])
+    wc.to_file('topwords.png')
 
 if __name__ == "__main__":
     with open(MODEL_FILEPATH) as model_file:
